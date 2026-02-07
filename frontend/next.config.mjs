d688cedf21c40252e58f2600f8e8824f5198a3bf
Deepak Chauhan <debugrror@gmail.com>
2026-02-08 02:02:57 +0530

    feat: Add baseUrl to tsconfig.json for improved module resolution.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -6,12 +6,17 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/api/:path*', // Proxy to Backend
+        destination: 'http://localhost:5002/api/:path*', // Proxy to Backend
       },
     ];
   },
   images: {
     domains: ['localhost'],
+  },
+
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 };
 
